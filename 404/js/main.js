8e01a2c6ed0bb17719d937cefe41fada8febbb1c
nikhi1g <2nikhilg@gmail.com>
2026-09-14 00:46:13 -0700

    404: wire the fishing module

--- 404/js/main.js
+++ 404/js/main.js
@@ -8,6 +8,8 @@ import {createGait} from './gait.js';
 import {createDamage} from './damage.js';
 import {createStairs} from './stairs.js';
 import {createArrow} from './arrow.js';
+import {createSaw} from './saw.js';
+import {createFishing} from './fishing.js';
 import {createCreature} from './creature.js';
 
 initTheme();
@@ -21,7 +23,9 @@ const gait = createGait(dot);
 const damage = createDamage(dot);
 const stairs = createStairs(dot);
 const arrow = createArrow(dot);
-createCreature({dot, gait, damage, stairs, arrow}).start();
+const saw = createSaw(dot);
+const fishing = createFishing(dot);
+createCreature({dot, gait, damage, stairs, saw, fishing, arrow}).start();
 
 dot.start();
 
